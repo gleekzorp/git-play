e15cbedd75f650bff3945879772c372f61110013
Ben <benriveracoding@gmail.com>
2021-02-05 11:01:41 -0700

    add RNG generator and new lot file

---
+++ git-play-generator/src/weightedLot.js
@@ -0,0 +1,10 @@
+export function weightedSlots() {
+    let chances = Math.floor(Math.random() * 5)
+    if(chances === 1) {
+        return "You win"
+    }else{
+        return "try again"
+    }
+}
+
+console.log(weightedSlots())
