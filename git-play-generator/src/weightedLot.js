acc1d2ea894ded8b608a4c6ce5378899b17a828f
Ben <benriveracoding@gmail.com>
2021-02-05 11:45:12 -0700

    pulled updated code and changed let to const

--- git-play-generator/src/weightedLot.js
+++ git-play-generator/src/weightedLot.js
@@ -1,5 +1,5 @@
 export function weightedSlots() {
-    let chances = Math.floor(Math.random() * 5)
+    const chances = Math.floor(Math.random() * 5)
     if(chances === 1) {
         return "You win"
     }else{
